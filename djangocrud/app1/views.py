c8058b54c07374277ccff0851fea981bf9267c5c
HamzahSikandar <h.hamzahkhan111@gmail.com>
2022-08-10 23:56:37 -0700

    Initial Commit

---
+++ djangocrud/app1/views.py
@@ -0,0 +1,45 @@
+from multiprocessing import context
+from django.shortcuts import redirect, render
+from .forms import EmployeeForm
+from .models import Employee
+# Create your views here.
+def Home(request):
+    form=EmployeeForm()
+    if request.method=='POST':
+        form=EmployeeForm(request.POST)
+        form.save()
+        form=EmployeeForm()
+    
+    data=Employee.objects.all()
+
+    
+
+
+    context={
+        'form':form,
+        'data':data,
+    }
+    return render(request,'app1/index.html',context)
+
+# Delete View
+def Delete_record(request,id):
+    a=Employee.objects.get(pk=id)
+    a.delete()
+    return redirect('/')
+    
+
+# Update View
+def Update_Record(request,id):
+    if request.method=='POST':
+        data=Employee.objects.get(pk=id)
+        form=EmployeeForm(request.POST,instance=data)
+        if form.is_valid():
+            form.save()
+    else:
+
+        data=Employee.objects.get(pk=id)
+        form=EmployeeForm(instance=data)
+    context={
+        'form':form,
+    }
+    return render (request,'app1/update.html',context)
